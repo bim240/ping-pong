e0f95f2b851a2a7ebd85c5d6d6dd22a175464ea8
bim240 <bimlendu357@gmail.com>
2021-06-14 18:04:42 +0530

    display current player score

--- src/Components/Step2.js
+++ src/Components/Step2.js
@@ -1,5 +1,43 @@
+import { StyledStep2 } from "./StyledComponent";
+import { Button } from "antd";
+import { useState } from "react";
+
 const Step2 = (props) => {
-  return <>step2</>;
+  const currentPlayers = localStorage.getItem("currentPlayer");
+  const [playerScore, setPlayerScore] = useState(
+    localStorage.getItem([currentPlayers])?.split("_") || [0, 0]
+  );
+  const currentPlayerInfo = currentPlayers.split("_");
+
+  const setNewScore = (index) => {
+    let newState = [...playerScore].map((item, i) =>
+      i === index ? item + 1 : item
+    );
+    setPlayerScore(newState);
+    localStorage.setItem([currentPlayers], newState.join("_"));
+  };
+  return (
+    <StyledStep2>
+      {currentPlayerInfo.map((player, index) => {
+        return (
+          <div key={`${player}${index}`} className="single_plalyer_container">
+            <h2 className="player_name">{player}</h2>
+            <Button
+              type="primary"
+              className="add_wins_btn"
+              onClick={() => {
+                setNewScore(index);
+              }}>
+              {" "}
+              Add wins
+            </Button>
+            <h4 className="wins_text">Wins</h4>
+            <h3 className="wins">{playerScore[index]}</h3>
+          </div>
+        );
+      })}
+    </StyledStep2>
+  );
 };
 
 export default Step2;
--- src/Components/StyledComponent.js
+++ src/Components/StyledComponent.js
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
 export const StyledStep1 = styled.div``;
+
+export const StyledStep2 = styled.div`
+  .single_plalyer_container {
+    padding-bottom: 2rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    justify-items: center;
+    .player_name {
+      font-size: 2rem;
+      font-weight: 900;
+    }
+    .wins_text {
+      font-size: 1.5rem;
+    }
+    .wins {
+      font-size: 3rem;
+      font-weight: 900;
+    }
+  }
+`;
