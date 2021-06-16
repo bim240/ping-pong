4b5c748873333245b4e20498b3e012162601e7fe
bim240 <bimlendu357@gmail.com>
2021-06-16 14:49:55 +0530

    remove unused file and extra styling

--- src/Containers/PingPong/StyledComponents.js
+++ src/Containers/PingPong/StyledComponents.js
@@ -1,9 +1,8 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledPingPong = styled.section`
   max-width: 40rem;
   margin: 0 auto;
   height: 100vh;
-  border: 1px solid green;
   padding: 2rem;
 `;
